d2d36a664bc30b6a24adf04d90af6255f5f89dd1
pbohdanov <107268135+pbohdanov@users.noreply.github.com>
2023-06-20 10:58:45 +0300

    Update modal window in reviews section

--- src/js/leave-a-review-about-our-chocolate.js
+++ src/js/leave-a-review-about-our-chocolate.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[data-reviews-modal-open]"),
+    closeModalBtn: document.querySelector("[data-reviews-modal-close]"),
+    modal: document.querySelector("[data-reviews-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
